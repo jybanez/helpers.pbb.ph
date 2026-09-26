ad2618b0c07a55113f490af243f763102b5fbd8e
Jonathan Ybanez <j.ybanez@gmail.com>
2026-09-26 19:43:24 +0800

    Add canonical integration action icon

--- js/ui/ui.icons.js
+++ js/ui/ui.icons.js
@@ -1,4 +1,4 @@
-import { ICON_DEFINITIONS } from "./ui.icons.catalog.js?v=0.21.122";
+import { ICON_DEFINITIONS } from "./ui.icons.catalog.js?v=0.21.208";
 
 const SVG_NS = "http://www.w3.org/2000/svg";
 const definitions = new Map(Object.entries(ICON_DEFINITIONS));
